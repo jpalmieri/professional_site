c88909570f727156ef3ae42271e0b846e7e27ec8
Joe Palmieri <drown.your.watches@gmail.com>
2018-03-01 04:28:56 +0000

    Revert "Merge branch 'hugo' into 'master'"
    This reverts merge request !45

---
+++ app/assets/javascripts/static-site.js
@@ -0,0 +1,6 @@
+// Redirect to https (a.k.a., "poor man's force ssl")
+// https://about.gitlab.com/2016/04/11/tutorial-securing-your-gitlab-pages-with-tls-and-letsencrypt/#javascript-based-redirect
+var host = "www.jpalmieri.com";
+if ((host == window.location.host) && (window.location.protocol != 'https:')) {
+  window.location = window.location.toString().replace(/^http:/, "https:");
+}
